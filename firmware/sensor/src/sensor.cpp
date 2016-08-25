ad078d372ef3bf7b8b3d8c8f81b2e48e9707fc71
Severin Smith <severin@smallbutdigital.com>
2016-08-25 15:18:37 -0700

    Working comms with all the sensor

--- firmware/sensor/src/sensor.cpp
+++ firmware/sensor/src/sensor.cpp
@@ -1,10 +1,88 @@
 #include "sensor.h"
 
 void setup() { 
+    Serial.begin(9600);
+
     leds.begin();
     leds.show();
 
-    Serial.begin(9600);
+    pinMode(PIR_PIN, INPUT_PULLUP);
+
+#ifdef USE_IR_PROX
+    if (!pulse.isPresent()) {
+      Serial.print("No SI114x found on pin ");
+      Serial.println(SI114_PIN);
+    }
+    pulse.initPulsePlug(); //Initialize I2C bus
+    pulse.setLEDcurrents(6, 6, 6); //set prox sensor LED currents
+    pulse.setLEDdrive(1, 2, 4); //set which LEDs are active on which pulse
+#endif
+}
+
+int getRawWind() {
+    return analogRead(RAW_VOLT_ANALOG_WING_SENSOR_PIN);
+}
+
+int getRawWindTemp() {
+    return analogRead(TEMPERATURE_ANALOG_WIND_SENSOR_PIN);
+}
+
+
+void printProx(){
+    pulse.fetchLedData();
+
+    int red = pulse.ps1;
+    int IR1 = pulse.ps2;
+    int IR2 = pulse.ps3;
+    int total = red + IR1 + IR2;
+
+    Serial.print(red);
+    Serial.print("\t");
+    Serial.print(IR1);
+    Serial.print("\t");
+    Serial.print(IR2);
+    Serial.print("\t");
+    Serial.println((long)total);    
+    Serial.print("\t");
+}
+
+float getWind() {
+    // This code is from Modern Devices
+
+    //Wind sensor globals
+    const float zeroWindAdjustment =  .2; // negative numbers yield smaller wind speeds and vice versa.
+    int TMP_Therm_ADunits;  //temp termistor value from wind sensor
+    float RV_Wind_ADunits;    //RV output from wind sensor
+    float RV_Wind_Volts;
+    int TempCtimes100;
+    float zeroWind_ADunits;
+    float zeroWind_volts;
+    float WindSpeed_MPH;
+
+    TMP_Therm_ADunits = analogRead(TEMPERATURE_ANALOG_WIND_SENSOR_PIN);
+    RV_Wind_ADunits = analogRead(RAW_VOLT_ANALOG_WING_SENSOR_PIN);
+    RV_Wind_Volts = (RV_Wind_ADunits *  0.0048828125);
+
+    // these are all derived from regressions from raw data as such they depend on a lot of experimental factors
+    // such as accuracy of temp sensors, and voltage at the actual wind sensor, (wire losses) which were unaccouted for.
+    TempCtimes100 = (0.005 * ((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits)) - (16.862 * (float)TMP_Therm_ADunits) + 9075.4;
+
+    zeroWind_ADunits = -0.0006 * ((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits) + 1.0727 * (float)TMP_Therm_ADunits + 47.172; //  13.0C  553  482.39
+
+    zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment;
+
+    // This from a regression from data in the form of
+    // Vraw = V0 + b * WindSpeed ^ c
+    // V0 is zero wind at a particular temperature
+    // The constants b and c were determined by some Excel wrangling with the solver.
+
+    WindSpeed_MPH =  pow(((RV_Wind_Volts - zeroWind_volts) / .2300) , 2.7265);
+
+    return WindSpeed_MPH;
+}
+
+int pollUltrasonic(){
+    return analogRead(ULTRASONIC_ANALOG_PIN);
 }
 
 void setOnboardLEDs(uint8_t rValue, uint8_t gValue, uint8_t bValue) {
@@ -16,10 +94,11 @@ void setOnboardLEDs(uint8_t rValue, uint8_t gValue, uint8_t bValue) {
 }
 
 void loop() {
-    setOnboardLEDs(255, 0, 0);
-    Serial.println("R");
-    delay(1000);
-    setOnboardLEDs(100, 100, 100);
-    Serial.println("W");
-    delay(1000);
+    // Serial.println(getWind(), 2);
+    // Serial.print(getRawWind(), DEC);
+    // Serial.print(" - ");
+    // Serial.println(getRawWindTemp(), DEC);
+    // printProx();
+
+    delay(25);
 }
--- firmware/sensor/src/sensor.h
+++ firmware/sensor/src/sensor.h
@@ -1,9 +1,20 @@
 #include <avr/power.h>
 #include <Adafruit_NeoPixel.h>
 #include <SPI.h>
+#include <SI114.h>
+
+#define USE_IR_PROX
 
 #define NEO_PIXEL_PIN 8
-#define analogPinForRV  1 //Pin for raw voltage from wind sensor
-#define analogPinForTMP  2 //Pin for temp from wind sensor
+#define ULTRASONIC_ANALOG_PIN 17
+#define RAW_VOLT_ANALOG_WING_SENSOR_PIN 1
+#define TEMPERATURE_ANALOG_WIND_SENSOR_PIN 2
+#define PIR_PIN 20
+#define SI114_PIN 0
 
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(2, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+#ifdef USE_IR_PROX
+	PortI2C pulseI2C(SI114_PIN);
+	PulsePlug pulse(pulseI2C);
+#endif
